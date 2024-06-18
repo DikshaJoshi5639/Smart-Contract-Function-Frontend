9567ec2d46e51b02c5896782962f7fe62bbc0cca
Diksha Joshi <148073630+DikshaJoshi5639@users.noreply.github.com>
2024-06-18 18:36:28 +0530

    Update deploy.js

--- Scripts/deploy.js
+++ Scripts/deploy.js
@@ -1,36 +1,40 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const initBalance = 1;
-  const Assessment = await hre.ethers.getContractFactory("SmartContract");
-  const assessment = await Assessment.deploy(initBalance);
-  await assessment.deployed();
+    const initBalance = 1;
+    const Assessment = await hre.ethers.getContractFactory("Mytoken");
+    const assessment = await Assessment.deploy(ethers.utils.parseEther("1.0"));
+    await assessment.deployed();
 
-  assessment.DisplayAddress();
+    assessment.DisplayAddress();
 
-  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
+    console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
 
-  assessment.on("showAddress", (walAddress) => {
-    console.log(`Wallet Address: ${walAddress}`);
-  });
+    assessment.on("showAddress", (owner) => {
+        console.log(`msg.sender is : ${owner}`);
+    });
 
-  assessment.on("deposite", (deposit_val, Balance) => {
-    console.log(`New deposit: ${deposit_val} new Balance is : ${Balance} ETH`);
-  })
+    assessment.on("deposite", (deposit_val, Balance) => {
+        console.log(`New deposit: ${deposit_val} new Balance is : ${Balance} ETH`);
+    });
 
-  assessment.on("withdraw", (withdraw_val, Balance) => {
-    console.log(`New withdraw: ${withdraw_val} new Balance is : ${Balance} ETH`);
-  })
+    assessment.on("withdraw", (withdraw_val, Balance) => {
+        console.log(`New withdraw: ${withdraw_val} new Balance is : ${Balance} ETH`);
+    });
 
+    assessment.on("redeem", (amount) => {
+        console.log(`Redeemed amount: ${amount} ETH`);
+    });
+
+    assessment.on("transfer", (from, to, amount) => {
+        console.log(`Transferred ${amount} ETH from ${from} to ${to}`);
+    });
+
+    // Call the "redeem" function
+    await assessment.Redeem();
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
